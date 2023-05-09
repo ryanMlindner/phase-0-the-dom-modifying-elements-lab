78f1c6e6f4d38a3bbea0f4047262adb9ad0b2083
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-05-09 13:12:32 -0600

    uhhh npm broke halp

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Q is the champion";
+document.body.append(newHeader);
